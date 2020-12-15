d6e217e3056b58a2b95fc67bdfbb28b90b7951d1
lyjacky11 <lyjacky@live.com>
2020-12-14 19:43:58 -0500

    Added user widget info

--- src/components/UserWidget.jsx
+++ src/components/UserWidget.jsx
@@ -1,10 +1,17 @@
 import React from 'react';
 import './UserWidget.css';
 
+import { MdAccountCircle } from 'react-icons/md';
+
 function UserWidget() {
     return (
         <div className="userWidget">
-            <h3>User Widget</h3>
+            <MdAccountCircle className="userWidget__avatar" />
+            <div className="userWidget__info">
+                <h4 className="userWidget__displayName">Sample User</h4>
+                <h5 className="userWidget__username">@demo</h5>
+            </div>
+            
         </div>
     )
 }
