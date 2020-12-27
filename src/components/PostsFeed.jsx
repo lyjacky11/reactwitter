d3e9819f0cd65a3512a52f3debd1c74800dca539
lyjacky11 <lyjacky@live.com>
2020-12-27 14:19:33 -0500

    Implement infinite scroll (not working)

--- src/components/PostsFeed.jsx
+++ src/components/PostsFeed.jsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import { db } from '../firebase';
 import { MdMenu, MdSearch } from 'react-icons/md';
+import InfiniteScroll from 'react-infinite-scroller';
 import './PostsFeed.css';
 
 /* Components */
@@ -10,17 +11,34 @@ import Post from './Post';
 
 function PostsFeed({ title }) {
     const [posts, setPosts] = useState([]);
+    const [lastPost, setLastPost] = useState();
+    const [morePosts, setMorePosts] = useState(true);
+    const docLimit = 10;
+
+    const fetchMorePosts = () => {
+        db.collection("posts").orderBy("timestamp", "desc").limit(docLimit).startAfter(lastPost).onSnapshot(snapshot => {
+            setLastPost(snapshot.docs[snapshot.docs.length - 1]);
+            const newPosts = snapshot.docs.map(doc => (
+                { id: doc.id, post: doc.data() }
+            ));
+            setPosts(posts.concat(newPosts));
+        }).catch(error => {
+            setMorePosts(false);
+            console.log(morePosts);
+            console.log(error);
+        });
+    };
 
     useEffect(() => {
         document.title = `${title} | Reactwitter`;
-        db.collection("posts").orderBy("timestamp", "desc").limit(10).onSnapshot(snapshot => {
+
+        db.collection("posts").orderBy("timestamp", "desc").limit(docLimit).onSnapshot(snapshot => {
+            setLastPost(snapshot.docs[snapshot.docs.length - 1]);
             setPosts(snapshot.docs.map(doc => (
                 { id: doc.id, post: doc.data() }
             )))
         })
-    }, [title]);
 
-    useEffect(() => {
         const menuBtn = document.querySelector(".postsFeed__menu");
         const sidebar = document.querySelector(".sidebar");
 
@@ -32,7 +50,7 @@ function PostsFeed({ title }) {
                 sidebar.classList.add("open_menu");
             }
         });
-    }, []);
+    }, [title]);
 
     return (
         <div className="postsFeed">
@@ -49,16 +67,27 @@ function PostsFeed({ title }) {
                 </div>
                 <CreatePost />
             </div>
-            {
-                posts?.map(postInfo => (
-                    <Link key={postInfo.id} to={`/post/${postInfo.id}`}>
-                        <Post
-                            key={postInfo.id}
-                            post={postInfo.post}
-                        />
-                    </Link>
-                ))
-            }
+            <InfiniteScroll
+                className="scroller"
+                loadMore={fetchMorePosts}
+                hasMore={false}
+                loader={
+                    <div key={0} className="loader">
+                        <h6>Loading more posts...</h6>
+                    </div>
+                }
+            >
+                {
+                    posts?.map(postInfo => (
+                        <Link key={postInfo.id} to={`/post/${postInfo.id}`}>
+                            <Post
+                                key={postInfo.id}
+                                post={postInfo.post}
+                            />
+                        </Link>
+                    ))
+                }
+            </InfiniteScroll>
         </div>
     )
 }
